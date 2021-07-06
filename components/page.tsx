ebc385356c54432a7996a713a15c116deb52ce41
Michael <46134133+michaelketzer@users.noreply.github.com>
2021-07-06 19:51:21 +0200

    Added more routing stuff;

--- components/page.tsx
+++ components/page.tsx
@@ -1,23 +1,33 @@
 import { ReactElement, useContext, useMemo } from "react";
 
 import Route from './route';
+import { RouteIdent } from "./routing";
 import { RoutingContext } from "./routingContext";
 import { useRouter } from "next/dist/client/router";
+
+const routes: RouteIdent[] = ['home', 'other_page', 'just_another_page'];
 
 export default function Page(): ReactElement {
   const ctx = useContext(RoutingContext);
   const { locale, asPath } = useRouter();
 
   const altLocales = useMemo(() => ctx.alternateLanguages.filter((lang) => lang !== locale), [ctx.alternateLanguages, locale]);
+  const altRoutes = useMemo(() => routes.filter((route) => route !== ctx.currentRouteIdent), [ctx.currentRouteIdent]);
 
   return <main>
     <h2>Current route</h2>
     Locale: <b>{locale}</b>, RouteIdent: <b>{ctx.currentRouteIdent}</b>, Pathname: <b>{asPath}</b>
 
     <br /><br />
-    <h2>Alternative Locales for currentRouteIdent</h2>
+    <h2>Alternative Locales</h2>
     {altLocales.map((locale) => <Route key={locale} locale={locale}>
       <button>Switch to locale <b>{locale}</b></button>
+    </Route>)}
+
+    <br /><br />
+    <h2>Alternative routes</h2>
+    {altRoutes.map((route) => <Route key={route} routeIdent={route}>
+      <button>Switch to route <b>{route}</b></button>
     </Route>)}
 
 
@@ -37,5 +47,21 @@ export default function Page(): ReactElement {
     {ctx.alternateLanguages.map((locale) => <Route key={locale} locale={locale} routeIdent={'just_another_page'}>
       <button>Switch to routeIdent: <b>just_another_page</b>, locale: <b>{locale}</b></button>
     </Route>)}
+
+    <br /><br />
+    <br /><br />
+    <br /><br />
+
+    Source Code: <a href={'https://github.com/michaelketzer/nextjs-localized-routes'} target={'_blank'} rel={'noreferrer'}><button>Github</button></a>
+
+    <style jsx>{`
+      * {
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+      }
+      
+      button {
+        margin: 1rem;
+      }
+    `}</style>
   </main>
 }
